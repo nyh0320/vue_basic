f261a4a0493803ba9b150f11ff8d281fdb255804
nongyh <176457847@qq.com>
2021-07-07 17:35:41 +0800

    aaaaa

---
+++ 19_组件_单文件组件/App.vue
@@ -0,0 +1,18 @@
+/* 配置组件的模板结构
+    template只能有一个根标签，建议用div把所有组件包裹起来
+ */
+<template>
+    <div>
+        <School/>
+    </div>
+</template>
+
+/* 配置组件的数据交互 */
+<<script>
+    //引入School组件
+    import  School from './components/School.vue'
+
+    export default{
+        components:{School}
+    }
+</script>
---
+++ 19_组件_单文件组件/components/School.vue
@@ -0,0 +1,45 @@
+/* 
+    当前.vue文件不是一个组件，而是一个组件的基本配置
+ */
+
+/* 配置组件模板结构 */
+<template>
+    <div>
+        <h2 class="name">学校名：{{name}}</h2>
+        <h2 class="address" @click="showInfo">地址：{{address}}</h2>
+    </div>
+</template>>
+
+/* 配置组件数据、交互、事件 */
+<script>
+    //此处只是暴露了一个组件的配置，并没有去创建组件（只是准备了信息）
+    //因为没有调用vue.extend
+    export default{
+        //data中存放组件需要的数据
+        data(){
+            return{
+                name:'振华中学',
+                address:'中华路'
+            }
+        },
+        methods: {
+            showInfo(){
+                // 此时this指vc
+                alert(this.name)
+            }
+        },
+    }
+</script>
+
+/* 配置组件样式 */
+<style>
+    .name{
+        background-color: aqua;
+    }
+    .address{
+        background-color: aqua;
+    }
+</style>
+
+
+
